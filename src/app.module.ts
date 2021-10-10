bf6c6399235c5c81bd1e1bc0ed3ce3a7843057d6
changhoi <0417kch@naver.com>
2021-10-10 13:31:24 +0900

    feat(boot module): logger, api module, typeorm setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APIModule } from './api/api.module';
+import { ConfigsModule } from './core/configs/configs.module';
+import { ConfigsService } from './core/configs/configs.service';
+import { AccessLoggerMiddleware } from './core/middlewares/logger.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot()
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigsModule],
+      useFactory: (config: ConfigsService) => ({
+        type: 'mysql',
+        host: config.DB_URL,
+        port: config.DB_PORT,
+        database: config.DB_NAME,
+        username: config.DB_USER,
+        password: config.DB_PASSWD,
+        synchronize: true,
+      }),
+      inject: [ConfigsService],
+    }),
+    APIModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(AccessLoggerMiddleware).forRoutes('*');
+  }
+}
